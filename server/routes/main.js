f9d6dd3f26a7c3a00e660ec7a0a9dd11ea2f765d
Krrish Halder <krrish.code07@gmail.com>
2024-02-11 19:22:25 +0530

    Refactor main.js and index.ejs for pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 //* Routes
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    try {
+        const locals = {
+            title: "NodeJS Blog",
+            description: "This is a blog made with NodeJS and Express",
+        }
+        
+        let perPage = 5;
+        let page = req.query.page || 1;
 
-    const locals = {
-        title: "NodeJS Blog",
-        description: "This is a blog made with NodeJS and Express",
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }]).skip(perPage * page - perPage).limit(perPage).exec();
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+    } catch (error) {
+        console.log(error);
     }
 
-    res.render('index', { locals });
-})
-
+});
 module.exports = router;
